0425d7e2962151f9c7e30aa4bdddfc50e1c0a241
manager <2283856877@qq.com>
2016-04-21 20:14:29 +0800

    初始化创建项目

---
+++ 自定义流水布局/自定义流水布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义流水布局
+//
+//  Created by admin on 16/4/21.
+//  Copyright © 2016年 程涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
